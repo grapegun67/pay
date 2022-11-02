1948d43f6bd53d8d72162b1f38e9125a47795c30
chaosworld69 <chaosworld69@naver.com>
2022-11-02 17:38:22 +0900

    open api author success

--- src/main/java/pay/payment/controller/SendAuth.java
+++ src/main/java/pay/payment/controller/SendAuth.java
@@ -1,11 +1,12 @@
 package pay.payment.controller;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import pay.payment.AppConfig;
@@ -20,7 +21,7 @@ public class SendAuth {
     public String authFunction(String tmp)    {
         WebClient webClient = (WebClient) ac.getBean("webClient");
 
-        String block = webClient.get()
+        String block = webClient.post()
                 .uri("/oauth/2.0/token")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .retrieve()
@@ -33,20 +34,17 @@ public class SendAuth {
     }
 
     @GetMapping("/auth-return")
-    public String getAuth(@RequestBody AuthClass authClass){
-        log.info("gun_result: " + authClass.getClient_id());
+    public String getAuth(@ModelAttribute AuthClass authClass){
+        log.info("gun_result: {} {} {}", authClass.getCode(), authClass.getScope(), authClass.getState());
         return "ok";
     }
 
     @Getter
+    @Setter
     static class AuthClass {
         private String code;
-        private String client_id;
-        private String client_secret;
-        private String redirect_uri;
-        private String grant_type;
+        private String scope;
+        private String state;
     }
-
-
 
 }
